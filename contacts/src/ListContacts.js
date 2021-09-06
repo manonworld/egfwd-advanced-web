32d7411752ba73a58eb2e2b79af006bedf02883e
mosta <info@manonworld.de>
2021-09-06 14:44:31 +0200

    Added search functionality

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,31 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ListContacts extends Component {
 
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired,
+    };
+
+    state = {
+        query: ''
+    };
+
+    updateQuery = ( query ) => {
+        this.setState(() => ({
+            query: query.trim()
+        }));
+    }
+
+    filterContacts = ( ) => {
+        return this.state.query === ''
+        ? this.props.contacts
+        : this.props.contacts.filter((contact) => (
+            contact.name.toLowerCase().includes(this.state.query.toLowerCase())
+        ));
+    }
+
     render() {
         return (
-            <ol className="contact-list">
-                {
-                    this.props.contacts.map((contact) => (
-                        <li key={contact.id} className='contact-list-item'>
-                            <div className="contact-avatar" style={{
-                                backgroundImage: `url(${contact.avatarUrl})`
-                            }}>
+            <div className="list-contacts">
+                <div className="list-contacts-top">
+                    <input 
+                        className="search-contacts"
+                        type="text"
+                        value={this.state.query}
+                        onChange={( event ) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+                <ol className="contact-list">
+                    {
+                        this.filterContacts().map((contact) => (
+                            <li key={contact.id} className='contact-list-item'>
+                                <div className="contact-avatar" style={{
+                                    backgroundImage: `url(${contact.avatarUrl})`
+                                }}>
 
-                            </div>
-                            <div className="contact-details">
-                                <p>{contact.name}</p>
-                                <p>{contact.handle}</p>
-                            </div>
-                            <button 
-                                onClick={() => this.props.onDeleteContact(contact)}
-                                className="contact-remove">
-                                    Remove
-                            </button>
-                        </li>
-                    ))
-                }
-            </ol>
+                                </div>
+                                <div className="contact-details">
+                                    <p>{contact.name}</p>
+                                    <p>{contact.handle}</p>
+                                </div>
+                                <button 
+                                    onClick={() => this.props.onDeleteContact(contact)}
+                                    className="contact-remove">
+                                        Remove
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ol>
+            </div>
         );
     }
 
